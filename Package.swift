db76ee2fbdc9d95bb6c1c12f8db9eaa2e48bc0cd
Joseph Mattello <mail@joemattiello.com>
2024-08-03 18:57:42 -0400

    Use swiftgen from source

--- Package.swift
+++ Package.swift
@@ -7,7 +7,9 @@ let package = Package(
     .plugin(name: "SwiftGenPlugin", targets: ["SwiftGenPlugin"]),
     .plugin(name: "SwiftGen-Generate", targets: ["SwiftGen-Generate"])
   ],
-  dependencies: [],
+  dependencies: [
+    .package(url: "https://github.com/SwiftGen/SwiftGen.git", from: "6.6.3")
+  ],
   targets: [
     .plugin(
       name: "SwiftGenPlugin",
@@ -27,11 +29,11 @@ let package = Package(
       ),
       dependencies: ["swiftgen"]
     ),
-    .binaryTarget(
-      name: "swiftgen",
-      url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen-6.6.2.artifactbundle.zip",
-      checksum: "7586363e24edcf18c2da3ef90f379e9559c1453f48ef5e8fbc0b818fbbc3a045"
-    )
+//    .binaryTarget(
+//      name: "swiftgen",
+//      url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen-6.6.2.artifactbundle.zip",
+//      checksum: "7586363e24edcf18c2da3ef90f379e9559c1453f48ef5e8fbc0b818fbbc3a045"
+//    )
   ],
   swiftLanguageVersions: [.v5, .v6]
 )
